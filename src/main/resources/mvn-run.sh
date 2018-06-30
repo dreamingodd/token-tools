24e538844c2ae616d61490087aef048c2d44f048
YWD@MVC <memorywilllast@gmail.com>
2018-06-30 15:00:38 +0800

    Air Drop for FCoin voting - Address 3000 in 6 files.

--- src/main/resources/mvn-run.sh
+++ src/main/resources/mvn-run.sh
@@ -4,3 +4,4 @@ mvn clean package
 
 # Eth send (Attention: Win use ; - semicolon as separator while linux use : - colon)
 java -classpath "target/token-tools.jar:target/lib/*" com.temp.SendEth "0x20771815237d62BA03737123C96f90678c3Ade46" "0xAd837b3C9C34295e797D94fB421c4b9280fC6d1F" "0.00989"
+java -classpath "target/token-tools.jar:target/lib/*" com.temp.AirDrop "/opt/token-tools/src/test/resources/air-drop-FT-voting-1.csv" "0x20771815237d62BA03737123C96f90678c3Ade46" "0xb17df9a3b09583a9bdcf757d6367171476d4d8a3" "1"
