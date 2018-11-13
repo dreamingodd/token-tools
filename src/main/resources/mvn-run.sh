1cb8a4b8dd81c263755497ed9bb95aa3872db82c
YWD@MVC <memorywilllast@gmail.com>
2018-11-13 15:54:47 +0800

    Start the approve/transferFrom experiment.

--- src/main/resources/mvn-run.sh
+++ src/main/resources/mvn-run.sh
@@ -23,3 +23,6 @@ java -classpath "target/token-tools.jar:target/lib/*" com.temp.TransferToken "0x
 
 # hk-1
 java -classpath "target/token-tools.jar:target/lib/*" com.temp.AirDropWithoutSign "/opt/token-tools/src/test/resources/air-drop-FT-voting-4.csv" "0xb17df9a3b09583a9bdcf757d6367171476d4d8a3" "0xe4e73673d3511430024fe8a3d9fae6499ed4eaa0" "1"
+
+
+java -classpath "target/token-tools.jar:target/lib/*" com.temp.AirdropByContract "/opt/token-tools/src/test/resources/kushen-airdrop.csv" "0x3f81686a83B277eC4F781019De0280b6d6e017D3" "0x4824a7b64e3966b0133f4f4ffb1b9d6beb75fff7" 18 "0xd3a79de5B70D788bFf9Cc455Af80fC4dE1B8F940" "1600"
