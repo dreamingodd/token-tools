9cd626c56be781912367608ee570dc66a3f99fe2
YWD@MVC <memorywilllast@gmail.com>
2018-12-10 18:24:24 +0800

    Upgrade GetTetherBalance to locate specific address, Along with completed SendToAddress.

--- src/main/java/com/temp/btc/GetTetherBalance.java
+++ src/main/java/com/temp/btc/GetTetherBalance.java
@@ -8,12 +8,29 @@ import java.io.IOException;
 import java.util.List;
 
 public class GetTetherBalance extends BtcAction {
+
+    private static String address;
+
     public static void main(String[] args) throws BitcoindException, IOException, CommunicationException {
         List<TetherBalance> tetherBalanceList = getTetherBalance();
 
+        parseArgs(args);
         // Print
         for (TetherBalance tetherBalance : tetherBalanceList) {
-            System.out.println(tetherBalance);
+            if (address == null) {
+                System.out.println(tetherBalance);
+            } else {
+                if (address.equals(tetherBalance.getAddress())) {
+                    System.out.println(tetherBalance);
+                }
+            }
+        }
+    }
+
+
+    private static void parseArgs(String[] args) {
+        if (args.length > 0) {
+            address = args[0];
         }
     }
 }
--- src/main/java/com/temp/btc/ListUnspent.java
+++ src/main/java/com/temp/btc/ListUnspent.java
@@ -8,6 +8,10 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
+// mv9YDCjtqeJ6LaNyBtk21ALWkrpnm2RuKb
+// Output(super=OutputOverview(super=Entity(), txId=02feb8307c235f5436153b69967a3491a5f3f1ee3aea32d034bf4a53adf7874a, vOut=0),
+// address=mv9YDCjtqeJ6LaNyBtk21ALWkrpnm2RuKb, account=mv9YDCjtqeJ6LaNyBtk21ALWkrpnm2RuKb, scriptPubKey=76a914a07d0c2c76f68471b2f2dd3f8a63aad062769b7488ac,
+// redeemScript=null, amount=0.00006000, confirmations=0, spendable=true)
 public class ListUnspent extends BtcAction {
 
     private static List<String> addresses;
